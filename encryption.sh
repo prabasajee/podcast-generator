41a80bd276a04263c3fceca9122bc8c8455ca5d7
pirapakaran sajeevan <162146775+prabasajee@users.noreply.github.com>
2025-06-26 09:35:51 +0000

    change

--- encryption.sh
+++ encryption.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-echo"=============================="
+echo "=============================="
 
 git config --global user.name "${GITHUB_USER}"
 git config --global user.email "${GITHUB_EMAIL}"
 git config --global --add safe.directory /github/workspace
 
-python3 /urs/bin/feed.py
+python3 /usr/bin/feed.py
 
 git add . -A && git commit -m "Update feed" || echo "No changes to commit"
 
-git push origin main || echo "Push failed, check your credentials or network connection"    
-echo"=============================="
+git push origin main || echo "Push failed, check your credentials or network connection"
+echo "=============================="
